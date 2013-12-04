4e845daa8545625d4366c2dd2a32180fd605b775
= <=>
2013-12-04 11:30:36 +0200

    SinglyLinkedList implementation

---
+++ singly-linked-list.js
@@ -0,0 +1,21 @@
+function Node(name, next, data) {
+    this.name = name;
+    if (next !== null) {
+        this.next = new Node(data[next].name, data[next].next, data);
+    } else {
+        this.next = next;
+    }
+}
+
+var data = [
+    {'name': 'val1', 'next': 1},
+    {'name': 'val2', 'next': 2},
+    {'name': 'val3', 'next': null}
+];
+
+var node = new Node(data[0].name, data[0].next, data);
+//iterate nodes
+while (node != null) {
+    console.log(node.name);
+    node = node.next;
+}
